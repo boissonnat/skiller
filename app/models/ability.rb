d9c80e0b8a7b92491a6bae835a95a639a1f19b08
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2014-09-29 14:07:20 +0200

    Improve ability for default users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -24,6 +24,48 @@ class Ability
         o.users.include? user
       end
 
+      ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+      ##    Rights for DEFAULT_USER with an organization     ##
+      ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+      if user.organization
+
+        ## QUESTIONS
+        can :read, Question
+        can :list, Question
+        can :create, Question
+        can :update, Question do |question|
+          question.user && question.user.organization == user.organization
+        end
+        can :destroy, Question
+        can :add_to_organization_repository, Question do |question|
+          (!user.organization.questions.include? question) && !question.is_private
+        end
+        can :remove_from_organization_repository, Question do |question|
+          (user.organization.questions.include? question) && !question.is_private
+        end
+
+        ## QUIZZES
+        can :read, Quiz
+        can :list, Quiz
+        can :create, Quiz
+        can :update, Quiz
+        can :destroy, Quiz
+
+        ## QUIZ_QUESTIONS
+        can :set_as_correct, QuizQuestion do |q|
+          !q.is_correct
+        end
+        can :set_as_incorrect, QuizQuestion do |q|
+          q.is_correct
+        end
+
+        ## CANDIDATES
+        can :update, Candidate do |candidate|
+          candidate.organization == user.organization
+        end
+
+      end
+
     end
 
     ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
@@ -33,8 +75,6 @@ class Ability
 
       ## ORGANIZATION
       can :update, Organization
-      can :parse_markdown, Organization
-
 
       ## QUESTIONS
       can :read, Question
